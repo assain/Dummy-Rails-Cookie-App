9882332e00739bb738c9fde851b5b000c8b61769
Assain <assainjaleel20@gmail.com>
2018-07-25 00:36:30 +0530

    Read 6-0 cookies on 5-2-Stable
    Note: You'll not be able to read 6.0 cookies when use_cookies_with_metadata
    is true in Rails 5-2. However, you can cookies that are written with the config
    set to false.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   def show_cookies
+    # use_cookies_with_metadata = false
+    # cookies.encrypted[:version] = { value: "Rails 6.0 cookie, use_cookies_with_metadata is false", expires: 1.year }
+    cookies[:version] = "Zhzsp7XJoLPW2W8ZFtNq297HGyWxi6qzSV9pVZzfZG+bCdHTjmXUbFdzM0OUFhGCdsgTVJ/F--4YC8nq8raokRsWMJ--CWMOFeUOnMhPS96FHMu6Kg=="
+    @version = cookies.encrypted[:version]
+
+    # use_cookies_with_metadata = true
+    # cookies.encrypted[:favorite] = { value: "Rails 6.0 Choco Chip Cookie, use_cookies_with_metadata is true", expires: 1.year }
+    cookies[:favorite] = "P27ysYEv9EA6oH1N3GZQA3HBDNDcVdxGF1pK3Gox8JoI3LmG7IyRqERhCllJh0tm9Uv2oGZqO7nnoNRTHxvFmcnrhcYBtzcy9deUUrU5Ufr3hU0Hi2J0aTWzhIx4xATDjtIVC0lMTujaBx0f3DD74pMdOaorD6b7jUit1CsKetfNfRJdxvf+2a0MZQBcrNMnFkbfBXujoXRYevzdBkJTrBneS0ZWGZsmNDQ=--+SFhhxaCx8KfDdki--RRLeSoky/DGqLf5apfiEbw=="
+    @not_readable_purpose_checking_on = cookies.encrypted[:favorite]
   end
 end
